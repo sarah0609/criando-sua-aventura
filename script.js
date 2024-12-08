3fa275f439a8ec457c5d0c377cca96b748219382
sarah0609 <sarah.pinto@escola.pr.gov.br>
2024-12-08 19:34:33 -0300

    Add files via upload
    projeto-final

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+ button.addEventListener('click',function( ) 
+ { const atual=document.querySelector}('.ativo')
+const avanca = document.querySelectorAll('.btn-proximo');
+
+atual.classlist.remove'(.ativo');
+ document.getElementById(proximoPasso).classList.add('ativo');
+ 
